b9e0e835ada6d5d88d0a1e154892cd6bd31bb992
davidherrera07 <davidherreralijeron07@gmail.com>
2026-09-22 13:24:21 +0200

    Add files via upload

---
+++ tema1/Traducciones.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("English\t\tInglés");
+        System.out.println("House\t\tCasa");
+        System.out.println("Dog\t\tPerro");
+        System.out.println("Cat\t\tGato");
+        System.out.println("Book\t\tLibro");
+        System.out.println("Water\t\tAgua");
+        System.out.println("Sun\t\tSol");
+        System.out.println("Moon\t\tLuna");
+        System.out.println("School\t\tEscuela");
+        System.out.println("Friend\t\tAmigo");
+        System.out.println("Food\t\tComida");
+    }
+}
