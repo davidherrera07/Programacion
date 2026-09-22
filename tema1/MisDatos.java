4903bbbc7a7b99dbd4c0598e9cc2ed40eccf24df
davidherrera07 <davidherreralijeron07@gmail.com>
2026-09-22 13:14:29 +0200

    Add files via upload

---
+++ tema1/MisDatos.java
@@ -0,0 +1,8 @@
+public class MisDatos {
+    public static void main(String[] args) {
+        // Este es mi primer programa
+        System.out.println("¡Hola me llamo David!");
+        System.out.println("Mi dirección es Calle Málaga");
+        System.out.println("Mi número de teléfono es 642868424");
+    }
+}
