98d6e6f1a3651e91e581b4ff1baadc64986da74e
davidherrera07 <davidherreralijeron07@gmail.com>
2026-09-22 13:52:39 +0200

    Add files via upload

---
+++ tema1/MiHorario.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hora\t\tLunes\t\tMartes\t\tMiércoles\tJueves\t\tViernes");
+        System.out.println("08:00-09:00\tLLMM\tBD\t\tSistemas informáticos\tProgramación\t\tED");
+        System.out.println("09:00-10:00\tLLMM\t\tBD\tSistemas informáticos\t\tProgramación\t\tBD");
+        System.out.println("10:00-11:00\tSistemas informáticos\t\tSostenibilidad\tIPE\tLLMM\t\BD");
+        System.out.println("11:00-11:30\tRECREO\t\tRECREO\t\tRECREO\t\tRECREO\t\tRECREO");
+        System.out.println("11:30-12:30\tSistemas informáticos\t\tED\t\tIPE\tSistemas informáticos\tIPE");
+        System.out.println("12:30-13:30\tProgramación\tProgramación\t\tDigitalización\t\tBD\t\tProgramación");
+		System.out.println("13:30-14:30\tProgramación\tProgramación\t\tED\t\tBD\t\tProgramación");
+    }
+}
+
